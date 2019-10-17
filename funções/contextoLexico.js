fde5222a76109543d0c39341b29b48b473ae6cf5
Lakshamana <guitrompa1@gmail.com>
2019-10-16 22:06:26 -0300

    add this.fn

--- funções/contextoLexico.js
+++ funções/contextoLexico.js
@@ -4,10 +4,13 @@ function fn() {
     console.log(valor)
 }
 
+this.fn = fn
+
 const that = this
 function exec(){
     const valor = 'Local'
     that.fn()
+    console.log(that)
     let fn = function() {
         console.log(valor)
     }
